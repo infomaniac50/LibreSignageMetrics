7ee24694961c54bbe9e6fbdf7f4da2b9f3369eb5
Derek Chafin <infomaniac50@gmail.com>
2019-04-11 04:37:18 -0500

    Rename makeDOT.py to dependencies2dot.py

--- makeGML.sh
+++ makeGML.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python makeDOT.py dist/ 1 builds/includes.dot
+python dependencies2dot.py dist/ 1 builds/includes.dot
 gv2gml -o builds/includes.gml builds/includes.dot
 sed -i -e 's/name/label/' builds/includes.gml
